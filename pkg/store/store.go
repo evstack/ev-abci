593a48abc146a5655c6e72e1bdd50ca6b50ce58b
Julien Robert <julien@rbrt.fr>
2025-09-17 17:51:27 +0200

    fix empty state

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strconv"
 
@@ -45,7 +46,7 @@ func NewExecABCIStore(store ds.Batching) *Store {
 // When the state does not exist, it returns an empty state.
 func (s *Store) LoadState(ctx context.Context) (*cmtstate.State, error) {
 	data, err := s.prefixedStore.Get(ctx, ds.NewKey(stateKey))
-	if err != nil {
+	if err != nil && !errors.Is(err, ds.ErrNotFound) {
 		return nil, fmt.Errorf("failed to get state metadata: %w", err)
 	}
 
