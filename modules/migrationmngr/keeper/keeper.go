6a7bf5220acf7a3a4f3d57d55bdc70fb61d70504
chatton <github.qpeyb@simplelogin.fr>
2025-10-10 12:42:09 +0100

    chore: disable ibc check

--- modules/migrationmngr/keeper/keeper.go
+++ modules/migrationmngr/keeper/keeper.go
@@ -122,23 +122,25 @@ func (k Keeper) IsMigrating(ctx context.Context) (start, end uint64, ok bool) {
 // In order to not import the IBC module, we only check if the IBC store exists,
 // but not the ibc params. This should be sufficient for our use case.
 func (k Keeper) isIBCEnabled(ctx context.Context) bool {
-	enabled := true
+	return false
 
-	if k.ibcStoreKey == nil {
-		return false
-	}
+	//enabled := true
 
-	sdkCtx := sdk.UnwrapSDKContext(ctx)
-
-	ms := sdkCtx.MultiStore().CacheMultiStore()
-	defer func() {
-		if r := recover(); r != nil {
-			// If we panic, it means the store does not exist, so IBC is not enabled.
-			enabled = false
-		}
-	}()
-	ms.GetKVStore(k.ibcStoreKey())
-
-	// has not panicked, so store exists
-	return enabled
+	//if k.ibcStoreKey == nil {
+	//	return false
+	//}
+	//
+	//sdkCtx := sdk.UnwrapSDKContext(ctx)
+	//
+	//ms := sdkCtx.MultiStore().CacheMultiStore()
+	//defer func() {
+	//	if r := recover(); r != nil {
+	//		// If we panic, it means the store does not exist, so IBC is not enabled.
+	//		enabled = false
+	//	}
+	//}()
+	//ms.GetKVStore(k.ibcStoreKey())
+	//
+	//// has not panicked, so store exists
+	//return enabled
 }
